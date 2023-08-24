63d2d388ba5afcabf3271963d7d1a75922914706
Elena Botkina <elena.botkina51@gmail.com>
2023-08-24 19:56:29 -0400

    Implement popup functionality

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const deleteButtonsArray = document.querySelectorAll('.card__delete-button');
 deleteButtonsArray.forEach(
   deleteButton => {
-    deleteButton.addEventListener(
-      'click', () => deleteButton.closest('.card').remove()
-    );
+    deleteButton.addEventListener('click', () => deleteButton.closest('.card').remove());
   }
 );
+
+const popup = document.querySelector('.popup');
+const editProfileButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+editProfileButton.addEventListener('click', () => popup.classList.add('popup_opened'));
+closePopupButton.addEventListener('click', () => popup.classList.remove('popup_opened'));
+
