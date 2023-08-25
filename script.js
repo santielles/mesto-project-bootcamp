ff3b9766c3ee9af3b9ff601f9896ceabdb134f75
Elena Botkina <elena.botkina51@gmail.com>
2023-08-24 21:11:19 -0400

    Implement like functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Delete Card Button
 const deleteButtonsArray = document.querySelectorAll('.card__delete-button');
 deleteButtonsArray.forEach(
   deleteButton => {
@@ -5,9 +6,23 @@ deleteButtonsArray.forEach(
   }
 );
 
+// Popup
 const popup = document.querySelector('.popup');
 const editProfileButton = document.querySelector('.profile__edit-button');
 const closePopupButton = document.querySelector('.popup__close-button');
 editProfileButton.addEventListener('click', () => popup.classList.add('popup_opened'));
 closePopupButton.addEventListener('click', () => popup.classList.remove('popup_opened'));
 
+// Like Button
+const likeButtonArray = document.querySelectorAll('.card__like');
+likeButtonArray.forEach(
+  likeButton => {
+    likeButton.addEventListener('click', () => {
+      if (likeButton.classList.contains('card__like_mode-active')) {
+        likeButton.classList.remove('card__like_mode-active')
+      } else {
+        likeButton.classList.add('card__like_mode-active')
+      };
+    });
+  }
+)
