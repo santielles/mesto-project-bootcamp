f0b2ac4bbef75c807aabba5d103741f86f4dbcd6
Elena Botkina <elena.botkina51@gmail.com>
2023-09-03 23:54:53 -0400

    Fix duplicated EventListeners

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
+    filename: 'main.js',
     publicPath: '',
   },
   mode: 'development',
